286dfa96e5f2db931d8a199e6179ad8bff71ddf1
Brendan O'Dea <brendan.ode4@gmail.com>
2018-01-30 20:26:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,6 @@
 def my_collect(array)
   i = 0
-  new_array = []
-  while i < array.length
-    new_array << yield(array[i])
-    i += 1
-  end
-  new_array
+
+  array.collect{|item| yield(item)}
+
 end
